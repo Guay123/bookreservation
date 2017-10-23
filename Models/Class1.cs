27d5f2e175d7fa180f1d383e689c00639c04f801
Guay123 <Guayanthony123@gmail.com>
2017-10-23 12:33:17 +0800

    Add files via upload

---
+++ Models/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace mess.Models
+{
+    public class Class1
+    {
+        [Required(ErrorMessage = "Enter ID Number")]
+        [RegularExpression("([1-9][0-9]*)", ErrorMessage = "Numbers only please")]
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Enter First Name")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
+        public String fname { get; set; }
+        [Required(ErrorMessage = "Enter Last Name")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
+        public String lname { get; set; }
+        [Required(ErrorMessage = "Please Enter Username")]
+        public String Uname { get; set; }
+        [Required(ErrorMessage = "Please Password")]
+        public String Pword { get; set; }
+    }
+}
